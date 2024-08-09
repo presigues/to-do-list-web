be000254621597989ad73e04335f6643bd9ffcda
pretzel <132411345+presigues@users.noreply.github.com>
2024-08-09 17:11:46 -0400

    update

--- register.js
+++ register.js
@@ -1,21 +1,51 @@
- // Import the functions you need from the SDKs you need
- import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
- import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-analytics.js";
- // TODO: Add SDKs for Firebase products that you want to use
- // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-analytics.js";
 
- // Your web app's Firebase configuration
- // For Firebase JS SDK v7.20.0 and later, measurementId is optional
- const firebaseConfig = {
-   apiKey: "AIzaSyCIrzu4NcaGj86E0NzpYL-M0Hs86DCWJqY",
-   authDomain: "to-do-list-web-a60e3.firebaseapp.com",
-   projectId: "to-do-list-web-a60e3",
-   storageBucket: "to-do-list-web-a60e3.appspot.com",
-   messagingSenderId: "618176734239",
-   appId: "1:618176734239:web:741673d8c736f4da9e212a",
-   measurementId: "G-96SRZ18V1D"
- };
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
- // Initialize Firebase
- const app = initializeApp(firebaseConfig);
- const analytics = getAnalytics(app);
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCIrzu4NcaGj86E0NzpYL-M0Hs86DCWJqY",
+    authDomain: "to-do-list-web-a60e3.firebaseapp.com",
+    projectId: "to-do-list-web-a60e3",
+    storageBucket: "to-do-list-web-a60e3.appspot.com",
+    messagingSenderId: "618176734239",
+    appId: "1:618176734239:web:741673d8c736f4da9e212a",
+    measurementId: "G-96SRZ18V1D"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+
+
+const submit = document.getElementById('submit');
+submit.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('pass').value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed up 
+            const user = userCredential.user;
+            alert("Creating Account....");
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert(errorMessage);
+            // ..
+        });
+
+})
