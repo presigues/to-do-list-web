52bd6cfbd25908355edd2d8ad4dc1da94e7d5dc9
pretzel <132411345+presigues@users.noreply.github.com>
2024-08-11 16:27:38 -0400

    update

---
+++ settinghandler.js
@@ -0,0 +1,12 @@
+const THEME_TAG = 1;
+
+switch(THEME_TAG){
+    case 1:
+        break;
+        case 2:
+            break;
+            case 3:
+                break;
+                case 4:
+                    break;
+}
