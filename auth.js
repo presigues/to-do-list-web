8a47aa03ec9a580c62b8c39b78f8c70efcd99b94
pretzel <132411345+presigues@users.noreply.github.com>
2024-08-12 15:44:05 -0400

    update

--- auth.js
+++ auth.js
@@ -1,35 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-analytics.js";
-
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyCIrzu4NcaGj86E0NzpYL-M0Hs86DCWJqY",
-    authDomain: "to-do-list-web-a60e3.firebaseapp.com",
-    projectId: "to-do-list-web-a60e3",
-    storageBucket: "to-do-list-web-a60e3.appspot.com",
-    messagingSenderId: "618176734239",
-    appId: "1:618176734239:web:741673d8c736f4da9e212a",
-    measurementId: "G-96SRZ18V1D"
-    
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
-
-const emailerror ="auth/email-already-exists";
-
-
 
 
 
@@ -41,7 +9,9 @@ submit.addEventListener("click", function (event) {
     const email = document.getElementById('email').value;
     const password = document.getElementById('pass').value;
 
-    createUserWithEmailAndPassword(auth, email, password)
+    console.log(email);
+
+ /*   createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             // Signed up 
             const user = userCredential.user;
@@ -54,24 +24,20 @@ submit.addEventListener("click", function (event) {
         .catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
-            alert(errorMessage);
-
-            if(errorMessage==emailerror) {
-                document.getElementById("email").style.borderColor = "red";
-            }
+            alert(errorMessage)
             // ..
         });
-
+*/
 })
-
+/*
 const submit2 = document.getElementById('submit2');
 submit2.addEventListener("click", function (e) {
     e.preventDefault();
 
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('pass').value;
+    const email2 = document.getElementById('emaillog').value;
+    const password2 = document.getElementById('passlog').value;
 
-    signInWithEmailAndPassword(auth, email, password)
+    signInWithEmailAndPassword(auth, email2, password2)
         .then((userCredential) => {
             // Signed up 
             const user = userCredential.user;     
@@ -89,4 +55,5 @@ submit2.addEventListener("click", function (e) {
         });
 
 })
+        */
 
