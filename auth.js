a9944ccc9586f153d7db970ca41cd8ea5c93474e
pretzel <132411345+presigues@users.noreply.github.com>
2024-08-12 13:11:34 -0400

    update

---
+++ auth.js
@@ -0,0 +1,130 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-analytics.js";
+
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCIrzu4NcaGj86E0NzpYL-M0Hs86DCWJqY",
+    authDomain: "to-do-list-web-a60e3.firebaseapp.com",
+    projectId: "to-do-list-web-a60e3",
+    storageBucket: "to-do-list-web-a60e3.appspot.com",
+    messagingSenderId: "618176734239",
+    appId: "1:618176734239:web:741673d8c736f4da9e212a",
+    measurementId: "G-96SRZ18V1D"
+    
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+const emailerror ="auth/email-already-exists";
+
+
+function tolog(){
+    document.getElementById("createbacklog").style.display = "none";
+    document.getElementById("cretitlelog").style.display = "none";
+    document.getElementById("emailtextlog").style.display = "none";
+    document.getElementById("emaillog").style.display = "none";
+    document.getElementById("passtextlog").style.display = "none";
+    document.getElementById("passlog").style.display = "none";
+    document.getElementById("submit2").style.display = "none";
+    document.getElementById("loginokilog").style.display = "none";
+
+    document.getElementById("createback").style.display = "block";
+    document.getElementById("cretitle").style.display = "block";
+    document.getElementById("emailtext").style.display = "block";
+    document.getElementById("email").style.display = "block";
+    document.getElementById("passtext").style.display = "block";
+    document.getElementById("pass").style.display = "block";
+    document.getElementById("submit2").style.display = "block";
+    document.getElementById("loginoki").style.display = "block";
+}
+function tosign(){
+    document.getElementById("createbacklog").style.display = "block";
+    document.getElementById("cretitlelog").style.display = "block";
+    document.getElementById("emailtextlog").style.display = "block";
+    document.getElementById("emaillog").style.display = "block";
+    document.getElementById("passtextlog").style.display = "block";
+    document.getElementById("passlog").style.display = "block";
+    document.getElementById("submit2").style.display = "block";
+    document.getElementById("loginokilog").style.display = "block";
+
+    document.getElementById("createback").style.display = "none";
+    document.getElementById("cretitle").style.display = "none";
+    document.getElementById("emailtext").style.display = "none";
+    document.getElementById("email").style.display = "none";
+    document.getElementById("passtext").style.display = "none";
+    document.getElementById("pass").style.display = "none";
+    document.getElementById("submit2").style.display = "none";
+    document.getElementById("loginoki").style.display = "none";
+}
+
+
+
+
+const submit = document.getElementById('submit');
+submit.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('pass').value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed up 
+            const user = userCredential.user;
+            alert("Creating Account....");
+            window.location.href = "info.html";
+            
+            // ...
+        })
+
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert(errorMessage);
+
+            if(errorMessage==emailerror) {
+                document.getElementById("email").style.borderColor = "red";
+            }
+            // ..
+        });
+
+})
+
+const submit2 = document.getElementById('submit2');
+submit2.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('pass').value;
+
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed up 
+            const user = userCredential.user;     
+            console.log(userCredential)
+            window.location.href = "main.html";
+            console.log("you are logged in as" + email);
+             
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert(errorMessage);
+            // ..
+        });
+
+})
+
